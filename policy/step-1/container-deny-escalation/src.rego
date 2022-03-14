75ce995e341756436ad9d4354b80d9d13d199b3c
Raif Ahmed <raif@SUSE-6067.localdomain>
2022-03-14 13:57:00 +0100

    Updates

--- policy/step-1/container-deny-escalation/src.rego
+++ policy/step-1/container-deny-escalation/src.rego
@@ -10,4 +10,7 @@ package container_deny_escalation
 import data.lib.common as common_lib
 
 violation[{"msg": msg, "details": {}}] {
+    c := common_lib.input_containers[_]
+	c.securityContext.privileged
+	msg := sprintf("Privileged container is not allowed: %v, securityContext: %v", [c.name, c.securityContext])
 }
--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,12 @@
 conftest fmt policy/
 # it is important to put the k8s conf first then paramters file as i parse based on index
+yq input/deployment/opa-example-deployment_valid.yaml     | conftest test --all-namespaces -o table --combine -p  policy/step-1 -
+yq input/deployment/opa-example-deployment_invalid.yaml   | conftest test --all-namespaces -o table --combine -p  policy/step-1 -
+
+yq input/namespace/opa-example-namespace_valid.yaml   data/input-labels.yaml   | conftest test --all-namespaces -o table --combine -p  policy/step-2 - 
+yq input/namespace/opa-example-namespace_invalid.yaml data/input-labels.yaml   | conftest test --all-namespaces -o table --combine -p  policy/step-2 -
+
+# all tests togther on all files
 yq input/deployment/opa-example-deployment_valid.yaml   data/input-labels.yaml   | conftest test --all-namespaces -o table --combine -p policy-lab-solution -
 yq input/deployment/opa-example-deployment_invalid.yaml data/input-labels.yaml   | conftest test --all-namespaces -o table --combine -p policy-lab-solution -
 
